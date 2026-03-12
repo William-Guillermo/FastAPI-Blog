cdfbc6441273f142b8e84f74ec054b31289871ec
william-guillermo <guillermodelapazw@gmail.com>
2026-03-12 17:39:37 -0600

    Endpoint basicos

--- main.py
+++ main.py
@@ -10,3 +10,28 @@ app = FastAPI(
 async def root():
     return {"message":"¡Hi,FastAPI!"}
 
+# Endpoint Get 
+@app.get("/items/")
+async def read_items():
+    return [{"name":"Item1"}, {"name":"Item2"}]
+
+# Endpoint Get con ID
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id, "name": f"Item {item_id}"}
+
+# Endpoint POST
+@app.post("/items/")
+async def create_item(name: str):
+    return {"name" :name, "created": True}
+
+# Endpoint PUT
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, name: str):
+    return {"item_id": item_id, "name": name, "update": True}
+
+# Endpoint DELETE
+@app.put("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"item_id": item_id, "deleted": True}
+
